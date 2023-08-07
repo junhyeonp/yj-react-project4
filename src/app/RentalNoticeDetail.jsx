c54e876c8f6ee8f89796b958fa65aea7d22fa705
junhyeonpark <psjking1229@naver.com>
2023-08-07 13:16:12 +0900

    commit

--- src/app/RentalNoticeDetail.jsx
+++ src/app/RentalNoticeDetail.jsx
@@ -4,32 +4,13 @@ import Layout from "../components/Layout";
 import LayoutContents from "../components/LayoutContents";
 import SubTitle from "../components/SubTitle";
 import { useQuery } from "react-query";
-import { rentalNoticeDetail, rentalNotices } from "../api";
+import { rentalNoticeDetail} from "../api";
 
 export default function RentalNoticeDetail() {
   const { id } = useParams();
   const { data } = useQuery(["noticeDetail", id], rentalNoticeDetail);
-  const { data: data2 } = useQuery("notices", rentalNotices);
-  console.log(data);
-  console.log("data2", data2);
+  
 
-  console.log(data2.notices);
-
-  const list = data2.notices.map((item, i) => {
-    if (item._id === data?.detail._id) {
-      const before = data2.notices[i - 1]
-        ? data2.notices[i - 1].title
-        : "처음 글입니다.";
-      const after = data2.notices[i + 1]
-        ? data2.notices[i + 1].title
-        : "마지막 글입니다.";
-      const good = [before, after];
-      console.log("이거", good);
-      return good;
-    }
-  });
-
-  console.log(list);
   return (
     <Layout>
       <SubTitle firstTitle="관리자모드" secondTitle="공지사항 상세보기" />
@@ -65,13 +46,13 @@ export default function RentalNoticeDetail() {
               <tr>
                 <td className="table_td border-l-0">이전글</td>
                 <td colSpan={3} className="table_td border-l-0">
-                  {list[0]}
+               
                 </td>
               </tr>
               <tr>
                 <td className="table_td border-l-0">다음글</td>
                 <td colSpan={3} className="table_td border-l-0">
-                  {list[1]}
+   
                 </td>
               </tr>
             </tbody>
