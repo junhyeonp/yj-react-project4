df220df17977c0b0e174c4f05e0800ceb0f4f78e
junhyeonpark <psjking1229@naver.com>
2023-08-07 13:39:27 +0900

    commit

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
-const BASE_PATH = process.env.NODE_ENV === "production" ? "https://port-0-yj-react-project4-back-eu1k2lll079pdn.sel4.cloudtype.app": "http://localhost:8080";
+// const BASE_PATH = process.env.NODE_ENV === "production" ? "https://port-0-yj-react-project4-back-eu1k2lll079pdn.sel4.cloudtype.app": "http://localhost:8080";
+const BASE_PATH = process.env.NODE_ENV === "production" ? "https://regal-bienenstitch-5b5903.netlify.app": "http://localhost:8080";
 
 export async function rentalNotices() {
   return await fetch(`http://localhost:8080/api/rental/notice`, {
